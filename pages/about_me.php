c26d5dd1a654082369f2526fc9c9e8a0ba576794
nwabuike <nwabuikeanthony@gmail.com>
2019-09-24 11:49:58 +0100

    inital commit

---
+++ pages/about_me.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html>
+<!-- saved from url=(0036)http://www.andy88jones.com/about.php -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    
+<link rel="icon" type="image/jpg" href="../img/nnamdi_shortcut.jpg" sizes="32x32">
+<link rel="icon" type="image/jpg" href="../img/nnamdi_shortcut.jpg" sizes="16x16">
+<!--
+<link rel="apple-touch-icon" sizes="180x180" href="http://www.andy88jones.com/nnamdi_shortcut.jpg">
+ <link rel="manifest" href="http://www.andy88jones.com/manifest.json">
+<link rel="mask-icon" href="http://www.andy88jones.com/safari-pinned-tab.svg" color="#5bbad5">
+-->
+<meta name="theme-color" content="#ffffff">
+
+    <title>Nnamdi | Visual Designer for Mobile and Web</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+<link href="../css/css" rel="stylesheet">
+<link href="../css/css(1)" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+
+</head>
+
+	<!-- Modal -->
+<body><div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    	<div class="modal-content">
+     
+			      <div class="modal-body">
+				      <h2>Get in touch<span class="red">.</span></h2>
+					  <div class="con-intro">
+						  <img srcset="../img/nnamdi_shortcut.jpg" width="45px" class="modalpic"><div class="hey-msg">
+						  <p>‹ Hey, thanks for reaching out, contact me below via email with as much detail as possible on what you need help with; i'll reply ASAP. </p>
+				      </div>
+				      
+				      
+	    		  </div>
+      	
+			       <div class="contact-links">
+				       <span class="meta">Drop me an intro email below;</span>
+				       <a href="mailto:nwabuikeanthony@gmail.com">nwabuikeanthony@gmail.com</a>
+				   </div>
+      
+      	</div>
+      
+	      <div class="modal-footer">
+	        <button type="button" class=" e-but" data-dismiss="modal">Close</button>
+	      </div>
+   
+   		</div>
+
+  </div>
+</div>
+
+
+	
+	
+
+
+	    
+	         <div class="availability">
+		         <div class="container">
+		         <div class="col-xs-2">
+	               <a href="../index.html" id="home" data-toggle="tooltip" title="" data-placement="right" data-original-title="Return Home"> <img srcset="../img/nnamdi_shortcut.jpg" width="45px"></a>
+		         </div>
+				 
+				 <div class="hidden-sm hidden-xs col-md-4 col-md-offset-2 ">
+		            <div id="avail-button">
+		              <p><span class="blink blink-speed light"></span>Currently accepting projects</p>
+	                </div>
+	            </div>
+	           
+
+		            
+		           <div class="col-xs-10 col-xs-offset-0 col-sm-9 col-sm-offset-1 col-md-2 col-md-offset-2">
+		               <a href="#" class="e-but" data-toggle="modal" data-target="#myModal">Get in touch</a>
+	            </div>
+		         </div>
+	         </div>
+	          
+
+            
+<div class="container">
+         
+   
+		
+
+
+				  
+<!--
+	   	    <div class="row spacing-half">
+	    </div>
+
+
+
+		<div class="row">
+            <div class="col-md-12">
+	            <img srcset="imgs/about-img.png 1000w, imgs/about-img@2x.png 2000w" width="100%" class="about-img" />
+	            <p class=" photo-meta meta">Me, the Mrs and our two pooches; Apollo & Buster</p>
+            </div>
+		</div>
+-->
+		
+			    <!-- Spacing -->
+	    <div class="row spacing-half">
+	    </div>
+
+
+		<div class="row">
+			<div class="col-md-12">
+			<img src="../img/apple.jpg" width="100%;" class="about-head">
+			</div>
+		</div>
+		
+			    <!-- Spacing -->
+	    <div class="row spacing-half">
+	    </div>
+
+		
+		<!-- Intro -->
+		<div class="row">
+		
+           
+           <div class="col-md-8 col-md-offset-2"> 
+	          
+					<h1><span class="about-hi"></span><span class="typed-cursor">|</span></h1>
+		
+					<p>I'm Nnamdi, a visual designer who spends his days in his office perfecting pixels and crunching code. Starting out in 2016, fresh faced from Technical Institution, I was a front end developer but always enjoyed designing more; alot more. As the years have gone on, I've been designing more and more and coding less and less, however I feel it's important to understand both as I can push the boundaries with design, knowing the limitations. Below are the services I've provided for clients around the world, I'd love to do the same for you too.</p>
+				
+		    	</div>
+		</div>
+		
+				    <!-- Spacing -->
+	    <div class="row spacing">
+	    </div>
+
+		
+		</div>
+		
+			<!-- Intro -->
+		<div class="row bio">
+			
+<div class="container">
+
+			<div class="col-xs-4 col-xs-offset-4 col-md-3 col-md-offset-1 test-pic">
+				<img src="../img/nnamdi.jpg" width="100%;">
+			</div>
+           
+           <div class="col-xs-10 col-xs-offset-1 col-md-7 "> 
+	          <p class="me-txt">"Working with Nnamdi was a superb experience, we bounced ideas back and forth from the off; the end product far exceeded our expectations."</p>
+	          <p class=" photo-meta meta" style="text-align: left;"><img srcset="./img/winsip.png" width="50px;">Gozie-Prep50ng</p>
+	          
+
+           </div>
+		</div>
+		</div>
+		
+		
+		<div class="container">
+		
+		  
+	    <!-- Spacing -->
+	    <div class="row spacing">
+	    </div>
+		
+				<!-- Intro -->
+		<div class="row center big-marg-bott">
+			 <div class="col-md-8 col-md-offset-2 "> 
+			<h1>How I can help you<span class="red">?</span></h1>
+			<p>No matter which stage your project is at, I will be able to help you along the way. Whether it's a start up that needs branding and identity or an established app which needs a UI update, see below for more detail.</p>
+			 </div>
+		</div>
+			 
+
+			 
+		<div class="row border-bottom">
+			 	  <div class="col-md-4"> 
+					 <img src="../img/ui-design@2x.png" width="100%;">
+           </div>
+           <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-1"> 
+					<h2>UI Design<span class="red">.</span></h2>
+					<p>Spending hours moving and shaping pixels in Sketch, I aim to provide beautiful designs to make your User Interfaces functional, yet a beautiful experience for your target audience.</p>
+           </div>
+           
+		</div>
+		
+		
+	
+		
+				<div class="row border-bottom">
+			
+			  <div class="col-md-4"> 
+					 <img src="../img/app-design@2x.png" width="100%;">
+           </div>
+           <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-1"> 
+					<h2>App Design<span class="red">.</span></h2>
+					<p>Currently my most frequent projects have been creating interfaces for mobile applications. Recently leaving my role as the senior designer at a prolific app development company has allowed me the opportunity to focus more on freelance work, the jobs I really love.</p>
+           </div>
+           
+      	</div>
+		
+
+		
+		
+		<div class="row border-bottom brandy">
+			 	  <div class="col-md-4"> 
+					 <img src="../img/branding@2x.png" width="100%;">
+           </div>
+            <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-1"> 
+					<h2>Branding<span class="red">.</span></h2>
+					<p>Starting by understanding the company's core values, I love to work with clients to create or refresh their image. Crafting numerous concepts until we both adore the final design.</p>
+           </div>
+		</div>
+		
+		
+
+		
+		
+		<div class="row">
+			
+			<div class="hidden-xs col-md-5"> 
+					 <img src="../img/front-end-development@2x.png" width="70%;">
+        
+    
+					<h2>Frontend Development<span class="red">.</span></h2>
+					<p>I've been a developer since I started, despite not doing it as frequently, I still enjoy bringing my own designs to life. This way I get to code my designs exactly how I intended them to look and nothing gets lost in translation.
+</p>
+       
+           
+		</div>
+		
+					    <!-- Spacing -->
+	   
+		
+				
+					<div class="hidden-xs col-md-5 col-md-offset-2"> 
+					 <img src="../img/wordpress@2x.png" width="70%;">
+           			
+           
+            
+					<h2>Wordpress Development<span class="red">.</span></h2>
+					<p>I've been using this CMS since back in the early days and it never ceases to amaze me with it's functionality. I still keep up to date with all the developments and use it should a client ever need a more complicated website coding.
+</p>
+      	
+		</div>
+			</div>
+					
+				
+		
+		
+		  
+	    <!-- Spacing -->
+	    <div class="row spacing hidden-xs">
+	    </div>
+	    
+	    
+		</div>
+	    <div class="about-proj">
+		    
+		        <!-- Spacing -->
+	    <div class="row spacing-half">
+	    </div>
+	    <div class="container">
+  	       <!-- Projects -->
+    	
+	    <div class="row proj">
+	      
+	                 <div class="col-md-8  small-marg-bott">
+	                <h2>Recent Projects<span class="red">.</span></h2>
+	                  <p>More projects and full case studies will be added to the website soon, <a href="mailto:nwabuikeanthony@gmail.com">please drop me an email</a> if you would like to discuss any of the projects or see more work.</p>
+	    
+	   
+	            </div>
+	    </div>
+	    
+	    <div class="row">    
+		      
+			<div class="col-md-5">	
+				<a href="../pages/Quizix.php"><img srcset="../img/Quiz.jpg" class="proji" width="100%"></a>
+			</div>
+			
+			<div class="col-md-7">	
+			<a href="#"><img srcset="../img/ticket_template.jpg" class="proji" width="100%"></a>
+			</div>
+			<!--
+			<div class="col-md-6">	
+			<a href="#"><img srcset="../img/Day&night.png" class="proji" width="100%"></a>
+			</div>
+			
+			<div class="col-xs-6 col-md-3">	
+			<a href="#"><img srcset="../img/stockboard.png" class="proji" width="100%"></a>
+			</div>
+			
+			<div class="col-xs-6 col-md-3">	
+			<a href="#"><img srcset="../img/winspire.png" class="proji" width="100%"></a>			
+			</div>
+	-->
+			
+	    </div>
+	        <!-- /.row -->
+
+	  
+	    <!-- Spacing -->
+	    <div class="row spacing-half">
+	    </div>
+	    </div>
+	    
+	    
+	    
+		</div>
+    
+
+
+    
+    
+    
+    
+    
+    	
+<div class="footbar">
+	
+
+			    
+<div class="container">
+  			<div class="row bbar">
+		            <div class="col-md-2 dets">
+			            <h5>Designing from:</h5>
+		                <p>Anambra,&nbsp; Nigeria</p>
+		            </div>
+		            <!---->
+		            <div class="col-md-2 col-md-offset-4 soc">
+		               <a href="#" target="_blank"><img srcset="../img/instagram@2x.png" width="30px"></a>
+		               <a href="#" target="_blank"><img srcset="../img/skype@2x.png" width="30px"></a>
+		               <a href="#" target="_blank"><img srcset="../img/dribbble@2x.png" width="30px"></a>
+		               <a href="#" target="_blank"><img srcset="../img/twitter@2x.png" width="30px"></a>
+		            </div>
+		            
+		            <div class="col-md-2">
+		                <a class="e-but" href="mailto:nwabuikeanthony@gmail.com">Get in touch</a>
+		            </div>
+        	</div>
+        <!-- /.row -->
+        
+        
+        
+        <div class="row meta-d">
+	    <div class="col-md-4">
+		    <span class="meta">Designed &amp; Built by yours truly with</span>
+	    </div>
+	    
+	    <div class="col-xs-12 col-xs-offset-0 col-md-6 col-md-offset-2">
+		    <span class="meta right">Contact me here for anything  <a href="mailto:nwabuikeanthony@gmail.com">08163778265</a></span>
+        </div>
+        <!--<div class="col-offset-md-2">
+	    <span class="np-copyright-text" style="color: white;">
+				© My Site</span>
+			<span class="sep"> | </span>
+            <?php echo date("Y");?>
+            <h2 class="site-info">Nnamdi Nwabuike</h2>
+        </div>-->
+        </div>
+        
+        
+        
+        
+</div>
+        
+    </div>  
+
+    
+    	    
+
+    
+    
+	
+	
+	
+    <!-- jQuery Version 1.11.1 -->
+    <script async="" src="./js/analytics.js.download"></script>
+    <script src="../js/jquery.js.download"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../js/bootstrap.min.js.download"></script>
+    
+    <!-- Dribbble -->
+    <script src="../js/jribbble.min.js.download"></script>
+    
+    <!-- TypeWriter -->
+	<script src="../js/typed.js.download" type="text/javascript"></script>
+    
+
+    
+    
+<script>
+  $(function(){
+      $(".element").typed({
+        strings: ["App Design", "UI Design", "Branding"],
+         loop: true,
+        typeSpeed: 50
+      });
+      
+         $(".about-hi").typed({
+        strings: ["Hello","Hullo","Hi!"],
+         loop: true,
+        typeSpeed: 30
+      });
+  });
+</script>
+   
+</body>
+
+</html>
